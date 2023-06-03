224a7c09557317000b76673cd3bfd6f24ce574ae
Anton Styazhkin <rudana089@gmail.com>
2023-06-03 22:08:55 +0300

    Complete step 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -58,10 +58,14 @@ class ContactForm extends Component {
 
 class ContactList extends Component {
   render() {
-    const { contacts } = this.props;
+    const { contacts, filter } = this.props;
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
     return (
       <ul>
-        {contacts.map(contact => (
+        {filteredContacts.map(contact => (
           <li key={contact.id}>
             {contact.name} - {contact.number}
           </li>
@@ -73,7 +77,15 @@ class ContactList extends Component {
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: '',
   };
 
   addContact = newContact => {
@@ -82,14 +94,24 @@ class App extends Component {
     }));
   };
 
+  handleFilterChange = e => {
+    this.setState({ filter: e.target.value });
+  };
+
   render() {
-    const { contacts } = this.state;
+    const { contacts, filter } = this.state;
     return (
       <div>
         <h1>Phonebook</h1>
         <ContactForm addContact={this.addContact} />
         <h2>Contacts</h2>
-        <ContactList contacts={contacts} />
+        <input
+          type="text"
+          value={filter}
+          onChange={this.handleFilterChange}
+          placeholder="Search contacts..."
+        />
+        <ContactList contacts={contacts} filter={filter} />
       </div>
     );
   }
