ff25425ee2947b7e74ea8f5a1d4dba35d311441d
Anton Styazhkin <rudana089@gmail.com>
2023-06-03 23:38:28 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,10 @@ import { v4 as uuid } from 'uuid';
 import styled from 'styled-components';
 
 const AppContainer = styled.div`
+  margin: 0 auto;
   text-align: center;
   padding: 20px;
+  width: 768px;
 `;
 
 const Title = styled.h1`
@@ -81,7 +83,12 @@ const FilterInput = styled.input`
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
 
